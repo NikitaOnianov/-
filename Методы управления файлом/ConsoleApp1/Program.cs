bc80a67fcee04f1e13969818aec8850ecdbe5764
NikitaOnianov <nikita.onianov@yandex.ru>
2024-12-18 18:12:11 +0300

    добавил четыре метода

---
+++ Методы управления файлом/ConsoleApp1/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool start = true;
+            while (start)
+            {
+                Console.WriteLine("Меню команд");
+                Console.WriteLine("1) Добавить файл");
+                Console.WriteLine("2) редактировать файл");
+                Console.WriteLine("3) вывести содержимое файла");
+                Console.WriteLine("4) удалить файл");
+                Console.WriteLine("5) Завершить сеанс");
+                Console.Write("Выберите номер действие: ");
+                int num = int.Parse(Console.ReadLine());
+                
+                try
+                {
+                    switch (num)
+                    {
+                        case 1: AddFile(); break;
+                        case 2: EditFile(); break;
+                        case 3: ReadFile(); break;
+                        case 4: DelFile(); break;
+                        case 5: start = false; break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Ошибка: " + e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Процесс завершён.");
+                }
+            }
+        }
+
+        static void AddFile()
+        {
+            Console.Write("Введите имя файла: ");
+            string namefile = Console.ReadLine();
+            string path = string.Format(@"C:\Users\VivoBook 15X\Desktop\-\Методы управления файлом\{0}.txt", namefile);
+            StreamWriter sw = new StreamWriter(path);
+            sw.Close();
+        }
+
+        static void EditFile()
+        {
+            // Открытие файла
+            Console.Write("Введите имя файла, который вы хотите редакторовать: ");
+            string namefile = Console.ReadLine();
+            string path = string.Format(@"C:\Users\VivoBook 15X\Desktop\-\Методы управления файлом\{0}.txt", namefile);
+            StreamWriter sw = new StreamWriter(path);
+            
+            // информация
+            Console.WriteLine("Можете переписывать файл. Когда закончите, напишите `выход()` на следующей строке.");
+
+
+            // Записываем
+            string line;
+            int i = 1;
+            while (true)
+            {
+                Console.Write("Наменование услуги: ");
+                string nameservise = Console.ReadLine();
+
+                Console.Write("Сколько длиться эта услуга в минутах: ");
+                int time = int.Parse(Console.ReadLine());
+
+                Console.Write("Стоимость услуги: ");
+                float price = float.Parse(Console.ReadLine());
+                // Добавление новой услуги.
+                line = string.Format("{0}. Наименование услуги: {1}, Длительность: {2}, Цена: {3}",i, nameservise, time, price);
+                sw.WriteLine(line);
+                i++;
+
+                // Выход
+                Console.Write("Желаете завершить работу? [Y/n]: ");
+                string o = Console.ReadLine();
+                if (o == "Y")
+                {
+                    // Сохранение и закрытие файла.
+                    sw.Close();
+                    break;
+                }
+            }
+        }
+
+        static void ReadFile()
+        {
+            // Открытие файла
+            Console.Write("Введите имя файла, который вы хотите посмотреть: ");
+            string namefile = Console.ReadLine();
+            string path = string.Format(@"C:\Users\VivoBook 15X\Desktop\-\Методы управления файлом\{0}.txt", namefile);
+            StreamReader sw = new StreamReader(path);
+
+            String line;
+            line = sw.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sw.ReadLine();
+            }
+            sw.Close();
+            Console.ReadLine();
+
+        }
+
+        static void DelFile()
+        {
+            Console.Write("Введите имя файла для удаления: ");
+            string namefile = Console.ReadLine();
+            string path = string.Format(@"C:\Users\VivoBook 15X\Desktop\-\Методы управления файлом\{0}.txt", namefile);
+            File.Delete(path);
+        }
+    }
+}
---
+++ Поток_данных_в_виде_непересекающихся_интервалов/ConsoleApp1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SummaryRanges
+{
+    private List<int[]> intervals;
+
+    public SummaryRanges()
+    {
+        intervals = new List<int[]>();
+    }
+
+    public void AddNum(int value)
+    {
+        int i = 0;
+
+        // Найти позицию для вставки
+        while (i < intervals.Count && intervals[i][1] < value)
+        {
+            i++;
+        }
+
+        // Если значение уже существует, ничего не делаем
+        if (i < intervals.Count && intervals[i][0] <= value && value <= intervals[i][1])
+        {
+            return;
+        }
+
+        // Проверяем и объединяем интервалы
+        if (i > 0 && intervals[i - 1][1] + 1 >= value)
+        {
+            intervals[i - 1][1] = Math.Max(intervals[i - 1][1], value);
+        }
+        else
+        {
+            intervals.Insert(i, new int[] { value, value });
+        }
+
+        // Объединяем с последующими интервалами
+        while (i + 1 < intervals.Count && intervals[i][1] + 1 >= intervals[i + 1][0])
+        {
+            intervals[i][1] = Math.Max(intervals[i][1], intervals[i + 1][1]);
+            intervals.RemoveAt(i + 1);
+        }
+    }
+
+    public int[][] GetIntervals()
+    {
+        return intervals.ToArray();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var commands = new string[] { "SummaryRanges", "addNum", "getIntervals", "addNum", "getIntervals", "addNum", "getIntervals", "addNum", "getIntervals", "addNum", "getIntervals" };
+        var values = new object[][] { new object[] { }, new object[] { 1 }, new object[] { }, new object[] { 3 }, new object[] { }, new object[] { 7 }, new object[] { }, new object[] { 2 }, new object[] { }, new object[] { 6 }, new object[] { } };
+
+        SummaryRanges summaryRanges = new SummaryRanges();
+        List<object> output = new List<object>();
+
+        foreach (var command in commands)
+        {
+            switch (command)
+            {
+                case "SummaryRanges":
+                    summaryRanges = new SummaryRanges();
+                    output.Add(null);
+                    break;
+                case "addNum":
+                    summaryRanges.AddNum((int)values[Array.IndexOf(commands, command)][0]);
+                    output.Add(null);
+                    break;
+                case "getIntervals":
+                    output.Add(summaryRanges.GetIntervals());
+                    break;
+            }
+        }
+
+        // Выводим результаты
+        foreach (var result in output)
+        {
+            if (result == null)
+                Console.WriteLine("null");
+            else
+                Console.WriteLine("[" + string.Join(", ", (int[][])result) + "]");
+        }
+    }
+}
